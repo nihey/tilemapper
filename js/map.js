9977429dd4567fbbc0830713a03a6d3162b1c871
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-04-07 21:14:06 -0300

    Add basic project boilerplate

---
+++ js/map.js
@@ -0,0 +1,41 @@
+window.Map = {
+ "height":10,
+ "layers":[
+        {
+         "data":[5, 1, 5, 1, 5, 1, 5, 1, 5, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+         "height":10,
+         "name":"Tile Layer 1",
+         "opacity":1,
+         "type":"tilelayer",
+         "visible":true,
+         "width":10,
+         "x":0,
+         "y":0
+        }],
+ "nextobjectid":1,
+ "orientation":"orthogonal",
+ "properties":
+    {
+
+    },
+ "renderorder":"right-down",
+ "tileheight":32,
+ "tilesets":[
+        {
+         "firstgid":1,
+         "imageheight":233,
+         "imagewidth":320,
+         "margin":0,
+         "name":"tiles00",
+         "properties":
+            {
+
+            },
+         "spacing":0,
+         "tileheight":32,
+         "tilewidth":32
+        }],
+ "tilewidth":32,
+ "version":1,
+ "width":10
+};
---
+++ js/tilemapper.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  var canvas = document.getElementById('canvas');
+  canvas.width = $('#tileset').width();
+  canvas.height = $('#tileset').height();
+});
