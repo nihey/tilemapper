badc42261d76c1437f949ac260f6b82cb1628de4
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-04-08 22:52:02 -0300

    Add clip number resolving function

--- js/tilemapper.js
+++ js/tilemapper.js
@@ -1,3 +1,15 @@
+function tilesetClip(tileset, number) {
+  var index = number - 1;
+  var row = Math.floor((index * tileset.tilewidth) / tileset.imagewidth);
+
+  return {
+    x: (index * tileset.tilewidth) % tileset.imagewidth,
+    y: (row * tileset.tileheight) % tileset.imageheight,
+    width: tileset.tilewidth,
+    height: tileset.tileheight,
+  }
+}
+
 $(document).ready(function() {
   var canvas = document.getElementById('canvas');
   canvas.width = $('#tileset').width();
