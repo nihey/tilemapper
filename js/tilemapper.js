c75ccf970213320a9effb3641e3d0eec45d80b68
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-04-08 23:10:00 -0300

    Draw first layer into the canvas

--- js/tilemapper.js
+++ js/tilemapper.js
@@ -12,6 +12,19 @@ function tilesetClip(tileset, number) {
 
 $(document).ready(function() {
   var canvas = document.getElementById('canvas');
-  canvas.width = $('#tileset').width();
-  canvas.height = $('#tileset').height();
+  var image = document.getElementById('tileset');
+  canvas.width = Map.width * Map.tilewidth;
+  canvas.height = Map.height * Map.tileheight;
+
+  var context = canvas.getContext('2d');
+
+  var tileset = Map.tilesets[0];
+  var layer = Map.layers[0];
+  for(var i = 0; i < layer.data.length; i++) {
+    var x = i % layer.width;
+    var y = Math.floor(i / layer.width);
+    var clip = tilesetClip(tileset, layer.data[i]);
+    context.drawImage(image, clip.x, clip.y, clip.width, clip.height,
+                      x * Map.tilewidth, y * Map.tileheight, Map.tilewidth, Map.tileheight);
+  }
 });
